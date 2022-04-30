f846444dab95b447487d4168517cc4e0a349fe9c
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-04-30 01:15:56 -0300

    steppet

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+// ------------------------------- Imports styles -------------------------------
+import { StepperContext } from './context/StepperContext';
+import Stepper from './components/Stepper';
+import StepperControl from './components/StepperControl';
+// ------------------------------- Imports components -------------------------------
+import Account from './components/steps/Account';
+import Details from './components/steps/Details';
+import Final from './components/steps/Final';
+import Payment from './components/steps/Payment';
 
 function App() {
+  const [currentStep, setCurrentStep] = useState(1);
+
+  const [userData, setUserData] = useState('');
+
+  const [finalData, setFinalData] = useState([]);
+
+  const steps = [
+    'Account Information',
+    'Personal Details',
+    'Complete'
+  ];
+
+  const displayStep = (step) => {
+    switch(step) {
+      case 1: 
+        return <Account />
+      case 2: 
+        return <Details />
+      case 3: 
+        return <Final />
+      default:
+    }
+  }
+
+  const handleClick = (direction) => {
+    let newStep = currentStep;
+
+    direction === "Next" ? newStep++ : newStep--;
+    // check if steps are within bounds
+    newStep > 0 && newStep <= steps.length && setCurrentStep(newStep);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="md:w-1/2 mx-auto shadow-xl rounded-2xl pb-2 bg-white">
+      {/* Stepper */}
+      <div className='container horizontal mt-5'>
+        <Stepper 
+        steps={steps}
+        currentStep={currentStep} />
+      </div>
+      {/* Disable Components */}
+        <div className='my-10 p-10'>
+          <StepperContext.Provider value={{
+            userData,
+            setUserData,
+            finalData,
+            setFinalData
+          }}>
+            {displayStep(currentStep)}
+          </StepperContext.Provider>
+        </div>
+      {/* Navigation Controls */}
+      <StepperControl 
+      handleClick={handleClick}
+      currentStep={currentStep}
+      steps={steps} />
     </div>
   );
 }
---
+++ src/components/steps/Account.jsx
@@ -0,0 +1,50 @@
+import {useContext} from 'react';
+import { StepperContext } from '../../context/StepperContext';
+
+const Account = () => {
+    const {userData, setUserData} = useContext(StepperContext);
+
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        setUserData({...userData, [name]: value });
+    }
+
+  return (
+    <div className='flex flex-col'>
+        <div className='w-full mx-2 flex-1'>
+            <div className='font-bold h-6 mt-3 text-gray-500 text-xs leading-8 uppercase'>
+                {""}
+                Nome do Usuário
+            </div>
+            <div className='bg-white my-2 p-1 flex border border-gray-200 rounded'>
+                <input
+                onChange={handleChange}
+                value={userData["username"] || ""}
+                name= "username"
+                placeholder='Nome do Usuário'
+                className='p-1 px-2 appearance-none outline-none w-full text-gray-800'
+                />
+            </div>
+        </div>
+
+        <div className='w-full mx-2 flex-1'>
+            <div className='font-bold h-6 mt-3 text-gray-500 text-xs leading-8 uppercase'>
+                {""}
+                Senha
+            </div>
+            <div className='bg-white my-2 p-1 flex border border-gray-200 rounded'>
+                <input
+                onChange={handleChange}
+                value={userData["password"] || ""}
+                name= "password"
+                placeholder='Senha'
+                type="password"
+                className='p-1 px-2 appearance-none outline-none w-full text-gray-800'
+                />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Account
